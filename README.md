adbd8f478f5959d4a437740be4c3178053a3e5af
René Kooi <renee@kooi.me>
2017-03-09 11:09:46 +0100

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+Room Settings Manager
+=====================
+
+Easily configure your room settings and style.
+
+## Installation
+
+If you do not have ExtPlug yet, get it [here][ExtPlug].
+
+You can install this plugin by going to your ExtPlug settings menu, pressing
+"Install Plugin", and entering this Plugin URL:
+
+```
+[YOUR HOST URL HERE]
+```
+
+## Building
+
+**Note: this section is intended for developers only.**
+
+This plugin uses the [ExtPlug CLI]. To build, run:
+
+```
+npm install
+npm run build
+```
+
+The built plugin will be saved at `build/room-settings-manager.js`.
+
+## License
+
+[MIT]
+
+[ExtPlug]: https://extplug.github.io/
+[ExtPlug CLI]: https://github.com/extplug/extplug-cli#readme
+[MIT]: ./LICENSE
---
+++ src/main.js
@@ -0,0 +1,42 @@
+import currentRoom from 'plug/models/currentRoom'
+import Plugin from 'extplug/Plugin'
+import { authenticate, httpTransport } from 'plug-auth-client'
+
+const API_URL = 'http://localhost:3000'
+
+const RoomSettingsManager = Plugin.extend({
+  name: 'Room Settings Manager',
+  description: 'Easily configure your room settings and style.',
+
+  enable () {
+    this.token = authenticate({
+      transport: httpTransport({ url: `${API_URL}/auth` })
+    }).then((result) => result.token)
+  },
+
+  disable () {
+  },
+
+  save () {
+    const myRoomSettings = {
+      stub: true
+    }
+
+    this.token.then((token) =>
+      fetch(`${API_URL}/${currentRoom.get('slug')}`, {
+        method: 'PUT',
+        headers: {
+          authorization: `JWT ${token}`,
+          accept: 'application/json',
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(myRoomSettings)
+      })
+    ).catch((err) => {
+      console.error(err)
+      throw err
+    })
+  }
+})
+
+export default RoomSettingsManager
